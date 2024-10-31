ab62b2b61283064437cc0911914c032e6dfda984
alineyHiga <alineyHiga>
2024-10-30 23:48:42 -0300

    add dto to fix foto null

---
+++ main/enum/tipoMedida.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class TipoMedida(Enum):
+    kg = 1
+    g = 2
+    unidade = 3
+    
+
--- main/model/GetProdutoDTO.py
+++ main/model/GetProdutoDTO.py
@@ -1,13 +1,13 @@
 # main/models.py
 from django.db import models
-from main.enum.tipoMedida import TipoMedida
-from produtoWebserver.settings import DEFAULT_FILE_STORAGE
 
-class Produto(models.Model):
+from main.enum.tipoMedida import TipoMedida
+
+class ProdutoDTO(models.Model):
     id_produto = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=200)
     descricao = models.TextField()
-    foto = models.ImageField(upload_to= DEFAULT_FILE_STORAGE)  # Requer configuração do servidor de mídia
+    foto = models.URLField()
     quantidade = models.DecimalField(max_digits=10, decimal_places=2)
     id_agricultor = models.IntegerField()
     classificacao = models.DecimalField(max_digits=3, decimal_places=1)
